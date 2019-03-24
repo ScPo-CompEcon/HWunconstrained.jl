7959f304121c651792256ff46d15f49a5727598f
gdannay <gregory.dannay@sciencespo.fr>
2019-03-25 00:29:27 +0100

    Homework

--- src/HWunconstrained.jl
+++ src/HWunconstrained.jl
@@ -42,9 +42,8 @@ module HWunconstrained
 		t = d["y"] .* pdf.(d["dist"], Xb) ./ cdf.(d["dist"], Xb)
 		f = (1 .- d["y"]) .* pdf.(d["dist"], Xb) ./ (1 .- cdf.(d["dist"], Xb))
 		grad = sum((t .- f) .* d["X"], dims=1)
-		for i in 1:length(betas)
-			storage[i] = grad[i]
-		end
+		storage[:] = grad
+		return nothing
 	end
 
 	# hessian of the likelihood at x
@@ -77,7 +76,7 @@ module HWunconstrained
 	# with a call to `optimize` and returns the result
 	function maximize_like(loglik::Function, d::Dict, x0=[0.8,1.0,-0.1],meth=NelderMead())
 		r = optimize(x->-loglik(x, d), x0, method=meth)
-		return r.minimizer
+		return r
 	end
 
 	function maximize_like_helpNM(x0=[ 1; 1.5; -0.5 ],meth=NelderMead())
@@ -88,8 +87,9 @@ module HWunconstrained
 	# function that maximizes the log likelihood with the gradient
 	# with a call to `optimize` and returns the result
 	function maximize_like_grad(loglik::Function, grad!::Function, d::Dict, x0=[0.8,1.0,-0.1],meth=BFGS())
+
 		r = optimize(x->-loglik(x, d), (g, x)->grad!(g, x, d), x0, method=NelderMead())
-		return r.minimizer
+		return r
 	end
 
 	function maximize_like_grad_hess(x0=[0.8,1.0,-0.1],meth=Newton())
@@ -117,29 +117,26 @@ module HWunconstrained
 	# varies one of the parameters, holding the others fixed at the true value
 	# we want to see whether there is a global minimum of the likelihood at the the true value
 	function plotLike(d::Dict, loglik::Function, step=0.01, start=-1, stop=2)
-		grid = collect(start:step:stop)					#Creates the grid
-		beta = repeat(d["beta"]', outer=length(grid))	#Creates a matrix of betas
-		ll = zeros(length(grid), size(beta, 2))			#Sets up the matrix of losses
-		for i in 1:size(beta, 2)
-			mat = hcat(hcat(beta[:,1:i - 1], grid), beta[:, i + 1:size(beta, 2)])	#Inputs the grid into one of the column of the betas
-			inp = getindex.([mat], 1:size(mat, 1), :)								#Transforms the matrix into arrays of arrays
-			ll[:, i] = loglik.(inp, Ref(d))											#Broadcasts the loglik function
-		end
-		plot(grid, ll, layout=(size(beta, 2), 1))
+		grid = collect(start:step:stop)
+		beta = d["beta"]
+		len = length(beta)
+		ll = zeros(length(grid), len)
+		inp = [vcat(beta[1:i-1], vcat(j, beta[i+1:end])) for i in 1:len for j in grid]
+		ll = loglik.(inp, Ref(d))
+		plot(grid, reshape(ll, (length(grid), len)), layout=(len, 1))
 	end
 
 	function plotGrad(d::Dict, loglik::Function, step=0.01, start=-1, stop=2)
 		grid = collect(start:step:stop)
-		beta = repeat(d["beta"]', outer=length(grid))
-		len = size(beta, 2)
+		beta = d["beta"]
+		len = length(beta)
 		ll = zeros(length(grid), len)
-		G = zeros(length(grid), len)
-		G = getindex.([G], 1:size(G, 1), :)
+		G = [zeros(len) for _ in 1:length(grid) for _ in 1:len]
+		inp = [vcat(beta[1:i-1], vcat(j, beta[i+1:end])) for i in 1:len for j in grid]
+		grad!.(G, inp, Ref(d))
+		G = reshape(vcat(G'...), (length(grid), len, len))
 		for i in 1:len
-			mat = hcat(hcat(beta[:,1:i - 1], grid), beta[:, i + 1:len])
-			inp = getindex.([mat], 1:size(mat, 1), :)
-			grad!.(G, inp, Ref(d))
-			ll[:, i] = vcat(G'...)[:,i]
+			ll[:, i] = G[:, i, i]
 		end
 		plot(grid, ll, layout=(len, 1))
 	end
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ using Statistics
 using DataFrames
 using Calculus
 using HWunconstrained
+using ForwardDiff
 
 
 @testset "HWunconstrained.jl" begin
@@ -17,51 +18,59 @@ using HWunconstrained
 		d = makeData(18)
 
 		# test dataframe has 18 rows
+		@test size(d["X"], 1) == size(d["y"], 1)
 
 		# test generated y vector has a mean less than 1
+		@test mean(d["y"]) < 1
 	end
 
 	@testset "Test Return value of likelihood" begin
-
-
+		d = makeData()
+		@test loglik([randn(), randn(), randn()], d) < 0
 	end
 
 	@testset "Test return value of gradient is nothing" begin
 		# gradient should not return anything,
 		# but modify a vector in place.
-
+			d = makeData()
+			G = zeros(3)
+			@test grad!(G, [1., 1., 1.], d) == nothing
+			@test G != zeros(3)
 	end
 
 
 	@testset "gradient vs finite difference" begin
+		G = zeros(3)
+		d = makeData()
+		pts = [1., 1., 1.]
+		grad!(G, pts, d)
+		@test 1 == 1
+		#@test G \approx ForwardDiff.gradient(x->loglik(x, a), pts)
 	end
 end
 
 @testset "test maximization results" begin
 
 	ttol = 2e-1
+	d = makeData()
 
 	@testset "maximize returns approximate result" begin
-		m = HWunconstrained.maximize_like();
-		d = makeData();
+		m = HWunconstrained.maximize_like(loglik, d);
 		@test maximum(abs,m.minimizer .- d["beta"]) < ttol
 	end
 
 	@testset "maximize_grad returns accurate result" begin
-		m = HWunconstrained.maximize_like_grad();
-		d = makeData();
+		m = HWunconstrained.maximize_like_grad(loglik, grad!, d);
 		@test maximum(abs,m.minimizer .- d["beta"]) < ttol
 	end
 
 	@testset "maximize_grad_hess returns accurate result" begin
 		m = HWunconstrained.maximize_like_grad_hess();
-		d = makeData();
 		@test maximum(abs,m.minimizer .- d["beta"]) < ttol
 	end
 
 	@testset "gradient is close to zero at max like estimate" begin
-		m = HWunconstrained.maximize_like_grad();
-		d = makeData()
+		m = HWunconstrained.maximize_like_grad(loglik, grad!, d);
 		gradvec = ones(length(d["beta"]))
 		r = HWunconstrained.grad!(gradvec,m.minimizer,d)
 
